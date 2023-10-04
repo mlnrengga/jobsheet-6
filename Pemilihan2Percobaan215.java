e6d726006da4f1a3772043a25bdfff2e2bd1fc24
mlnrengga <mlnrengga@gmail.com>
2023-10-04 23:41:40 +0700

    Tugas 2

---
+++ Pemilihan2Percobaan215.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan215 {
+public static void main(String[] args) {
+    Scanner input15 = new Scanner (System.in);
+    float totalSudut;
+
+    System.out.print("Masukkan Nilai Sudut 1: ");
+    float sudut1 = input15.nextFloat();
+    System.out.print("Masukkan Nilai Sudut 2: ");
+    float sudut2 = input15.nextFloat();
+    System.out.print("Masukkan Nilai Sudut 3: ");
+    float sudut3 = input15.nextFloat();
+
+    totalSudut = sudut1 + sudut2 + sudut3;
+
+    if (totalSudut == 180) {
+        if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) 
+            System.out.println("Segitiga Tersebut Adalah Segitiga Siku Siku");
+        else 
+            System.out.println("Segitiga Tersebut Adalah Bukan Segitiga Siku Siku"); 
+        }
+        input15.close();  
+}
+}
+
