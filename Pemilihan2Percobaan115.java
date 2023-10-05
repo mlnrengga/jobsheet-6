114c860af094fb788006f0d60469c4467046199d
mlnrengga <mlnrengga@gmail.com>
2023-10-05 22:25:05 +0700

    Add; Kelipatan 400

--- Pemilihan2Percobaan115.java
+++ Pemilihan2Percobaan115.java
@@ -8,9 +8,11 @@ public class Pemilihan2Percobaan115 {
     if ((tahun % 4) == 0) {
         if ((tahun % 100) != 0) {
         System.out.println("Tahun Kabisat");
-        }else {
-            System.out.println("Tahun 2100 Bukan Tahun Kabisat"); 
-        }
+     }else  
+        if ((tahun % 400) == 0) {
+        System.out.println("Tahun Kabisat"); 
+     }else
+        System.out.println("Bukan Tahun Kabisat");    
     } else  
         System.out.println("Bukan Tahun Kabisat");
     input15.close();
